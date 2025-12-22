cacf08265982531484698ce1a7c106eef67fe00b
FarrazID1 <farraz02@gmail.com>
2025-12-22 14:17:00 +0700

    chore: intial project setup

---
+++ main.py
@@ -0,0 +1,9 @@
+# define a main function
+def main():
+    print("Welcome to To-Do App")
+    #print("Press 1 to add a task")
+    #print("Press 2 to view your tasks")
+    #print("Press 3 to exit")
+
+if __name__ == "__main__":
+    main()
